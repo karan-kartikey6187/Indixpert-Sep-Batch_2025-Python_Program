6fb373b980c8d8bdbd25d29b77ad0e7c1faa7c9d
karan-kartikey6187 <karankartikey108@gmail.com>
2025-11-25 20:54:40 +0530

    updated

--- Standard Library/DataTimeNow.py
+++ Standard Library/DataTimeNow.py
@@ -10,3 +10,9 @@ print("30 Days Back From Now:",expected)
 
 now=datetime.datetime.now()
 print("Date Time:",now.strftime("%y/%m/%d,%H:%M:%S"))
+
+now=datetime.datetime.now()
+print("Date Time:",now.strftime("%Y/%b/%d,%H:%M:%S")) #small b for months like.jan feb mar 
+
+now=datetime.datetime.now()
+print("Date Time:",now.strftime("%Y/%B/%d,%H:%M:%S")) #capital B for Month Like. January february march
